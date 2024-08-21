d1e83965b0e73fb92af51105aadb5a2d286a50ee
Eduardo Ramón <edu.rz@hotmail.com>
2024-08-21 09:55:05 -0500

    detail screen

--- app/detail.tsx
+++ app/detail.tsx
@@ -6,21 +6,54 @@ import Colors from "@/constants/Colors";
 import { useState } from "react";
 
 export default function DetailScreen() {
+  const data = {
+    id: 1,
+    title: "Plan for a day",
+    description:
+      "A mysterious secret is here, from an unforgettable adventure to a crude and terrible experience.",
+    emoji: "",
+    favorite: false,
+    color: "#EB7A53",
+  };
+
   return (
-    <View style={styles.container}>
-      <View style={styles.options}></View>
+    <View style={[styles.container, {backgroundColor: data.color}]}>
+      <View style={[styles.options, {backgroundColor: data.color}]}></View>
 
       <Text style={styles.titleContainer}>
-        <Text style={styles.title}>My</Text>
-        <Text style={styles.title}>Cards</Text>
+        <Text style={styles.title}>{data.title}</Text>
       </Text>
 
-      
+      <Text style={styles.scratchContainer}>
+        <View style={styles.scratch}></View>
+      </Text>
+
+      <Text style={styles.description}>
+        <Text>{data.description}</Text>
+      </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  scratch: {
+    backgroundColor: "black",
+    borderRadius: 60,
+    width: 230,
+    height: 230,
+  },
+  scratchContainer: {
+    display: "flex",
+    justifyContent: "center",
+    paddingVertical: 60,
+  },
+  description: {
+    display: "flex",
+    textAlign: "center",
+    fontSize: 22,
+    paddingHorizontal: 10,
+    fontFamily: "Numans",
+  },
   options: {
     height: 60,
     backgroundColor: "black",
@@ -37,7 +70,7 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 56,
-    color: "white",
+    color: "black",
     fontFamily: "Numans",
   },
   separator: {
